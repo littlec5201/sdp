4a549425428d8ca6702180dc2e2dcd374b0d5ae8
littlec5201 <cjbensemann@gmail.com>
2016-09-25 12:10:16 +1300

    Add files via upload

---
+++ RegisterTest.java
@@ -0,0 +1,135 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tests.sdp.project;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Callum
+ */
+public class RegisterTest {
+    
+    public RegisterTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of setUp method, of class RegisterTest.
+     */
+    @Test
+    public void testSetUp() throws Exception {
+        System.out.println("setUp");
+        RegisterTest instance = new RegisterTest();
+        instance.setUp();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testUserAlreadyExists method, of class RegisterTest.
+     */
+    @Test
+    public void testUserAlreadyExists() {
+        System.out.println("testUserAlreadyExists");
+        RegisterTest instance = new RegisterTest();
+        instance.testUserAlreadyExists();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testShortUsername method, of class RegisterTest.
+     */
+    @Test
+    public void testShortUsername() {
+        System.out.println("testShortUsername");
+        RegisterTest instance = new RegisterTest();
+        instance.testShortUsername();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testShortPassword method, of class RegisterTest.
+     */
+    @Test
+    public void testShortPassword() {
+        System.out.println("testShortPassword");
+        RegisterTest instance = new RegisterTest();
+        instance.testShortPassword();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testPasswordNumbers method, of class RegisterTest.
+     */
+    @Test
+    public void testPasswordNumbers() {
+        System.out.println("testPasswordNumbers");
+        RegisterTest instance = new RegisterTest();
+        instance.testPasswordNumbers();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testPasswordLowerCase method, of class RegisterTest.
+     */
+    @Test
+    public void testPasswordLowerCase() {
+        System.out.println("testPasswordLowerCase");
+        RegisterTest instance = new RegisterTest();
+        instance.testPasswordLowerCase();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testPasswordUpperCase method, of class RegisterTest.
+     */
+    @Test
+    public void testPasswordUpperCase() {
+        System.out.println("testPasswordUpperCase");
+        RegisterTest instance = new RegisterTest();
+        instance.testPasswordUpperCase();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of test method, of class RegisterTest.
+     */
+    @Test
+    public void test() {
+        System.out.println("test");
+        RegisterTest instance = new RegisterTest();
+        instance.test();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+}
